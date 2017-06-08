7649ed484f89035beeb98ecab13b9c3619eda0be
Lucas Martins <lucasedu_vm@hotmail.com>
2017-06-08 15:28:55 -0300

    Test changes to git hub

--- arbvalue/wsgi.py
+++ arbvalue/wsgi.py
@@ -10,14 +10,16 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arbvalue.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
 from exchange.models import Exchange_Currency, Exchange_Pair, Exchange, Currency
 
 exchange = Exchange.objects.all()
 Currency = Currency.objects.all()
-
-print(exchange)
+exchange_currency = Exchange_Currency.objects.all()
+exchange_pair = Exchange_Pair.objects.all()
