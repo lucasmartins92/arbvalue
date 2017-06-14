bef224557a7ca8659111964d19381e397906e874
Lucas Martins <lucasedu_vm@hotmail.com>
2017-06-14 18:23:53 -0300

    Honcho configurado

--- exchange/tasks.py
+++ exchange/tasks.py
@@ -9,7 +9,7 @@ import time
 
 @shared_task
 def check_orderbook_db():
-    unix_old = time.time()-10*60
+    unix_old = time.time()-35*60
     old_data = Order_Book.objects.filter(unix__lte=unix_old)
     print("Quantidade de Dados Antigos: " + str(old_data.count()))
     old_data.delete()
@@ -64,8 +64,58 @@ def insert_negociecoins_db(exchange_pair, unix, orderbook):
             break
 
 #================================================================================================
+#FoxBit
+
+@shared_task
+def foxbit_orderbook():
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers = {'User-Agent': user_agent, }
+    orderbook = "https://api.blinktrade.com/api/v1/BRL/orderbook"
+    req = Request(orderbook, headers=headers)
+    response = urlopen(req)
+    if(response.getcode()==200):
+        orderbook = load(response)
+        unix = time.time()
+        exchange = Exchange.objects.get(code="FOX")
+        base = Currency.objects.get(code="BTC")
+        quote = Currency.objects.get(code="BRL")
+        exchange_pair = Exchange_Pair.objects.get(exchange=exchange, base=base, quote=quote)
+        insert_negociecoins_db(exchange_pair, unix, orderbook)
+
+@shared_task
+def insert_foxbit_db(exchange_pair, unix, orderbook):
+    Exchange_Pair.objects.filter()
+    bidbook = orderbook['bids']
+    askbook = orderbook['asks']
+    count = 1
+    for bid in bidbook:
+        if count <= 5:
+            count += 1
+            insert_bid = Order_Book(exchange_pair=exchange_pair,
+                                    unix=unix,
+                                    type='bid',
+                                    volume=bid[0],
+                                    price=bid[1])
+            insert_bid.save()
+        else:
+            break
+    count = 1
+    for ask in askbook:
+        if count <= 5:
+            count += 1
+            insert_ask = Order_Book(exchange_pair=exchange_pair,
+                                    unix=unix,
+                                    type='ask',
+                                    volume=ask[0],
+                                    price=ask[1])
+            insert_ask.save()
+        else:
+            break
+
+#================================================================================================
 
 @shared_task
 def api():
     check_orderbook_db.delay()
     negociecoins_orderbook.delay()
+    foxbit_orderbook.delay()
